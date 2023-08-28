7065274ff3a328d78648b030b2e1153d1f14aa88
ethan lane <ethan.lane@outlook.co.nz>
2023-08-28 20:28:40 +0100

    form lesson cards

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -2,29 +2,21 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="dropdown"
 export default class extends Controller {
-  static targets = ["dropdownButton", "dropdownList", "hiddenDropdown"]
+  static targets = ["dropdownButton", "dropdownList", "hiddenDropdown", "package", "hiddenPackage"]
 
+
+  packageType(event) {
+    const packageNumber = Number(event.target.attributes[0].value)
+    this.hiddenPackageTarget.value = packageNumber
+  }
 
   fire(event) {
     const listItem = this.dropdownListTarget
     const button = this.dropdownButtonTarget
-    // console.log(listItem)
-    // console.log(button)
-
     const clickedListItem = event.target;
-    console.log(clickedListItem);
-
-    // console.log(selectedText)
-
     button.innerText = clickedListItem.innerText
-
-    // this.hiddenDropdownTarget.value = button.innerText
-
     const listValue = Number(button.innerText.split(" ")[0])
-    // console.log(listValue)
     this.hiddenDropdownTarget.value = listValue
-
-    console.log(this.hiddenDropdownTarget.value)
   }
 
   connect() {
