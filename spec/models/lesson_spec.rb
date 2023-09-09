54caf37d407bcdf1916332de176b7a10eb37ebcf
ethan lane <ethan.lane@outlook.co.nz>
2023-09-09 20:15:59 +0100

    added custom descriptions to error messages throughout models

--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -5,10 +5,19 @@ RSpec.describe Lesson, type: :model do
   let(:lesson) {Lesson.new(name: "lesson_1", description: "intro_lesson", price: 45.00, duration: 60, user_id: user.id )}
 
   describe "validations" do
-    it { should validate_presence_of(:name) }
-    it { should validate_presence_of(:description) }
-    it { should validate_presence_of(:price) }
-    it { should validate_presence_of(:duration) }
+    it "should have a name" do
+      should validate_presence_of(:name)
+    end
+    it "should have a description" do
+      should validate_presence_of(:description)
+    end
+    it "should have a price" do
+      should validate_presence_of(:price)
+    end
+
+    it "should have a duration" do
+      should validate_presence_of(:duration)
+    end
 
     it "validates uniqueness of lesson name within the same user" do
       lesson_two = Lesson.new(name: "lesson_1", user_id: user.id)
@@ -19,8 +28,14 @@ RSpec.describe Lesson, type: :model do
   end
 
   describe 'associations' do
-    it { should belong_to(:user) }
-    it { should have_many(:slots) }
-    it { should have_many(:lesson_orders) }
+    it "should belong to a user" do
+      should belong_to(:user)
+    end
+    it "should have many slots" do
+      should have_many(:slots)
+    end
+    it "should have many lesson_orders" do
+      should have_many(:lesson_orders)
+    end
   end
 end
