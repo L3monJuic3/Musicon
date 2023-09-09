c16a5f46c6dc2984cff3858c0d455cab545c9839
ethan lane <ethan.lane@outlook.co.nz>
2023-09-09 17:38:56 +0100

    finished writing initial tests for slot model

---
+++ spec/models/slot_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe Slot, type: :model do
+  let(:user) { User.create(email: "test@email.com", password: "123456", phone_number: "07397282826", date_of_birth: Date.new) }
+  let(:lesson) { Lesson.create(name: "lesson_1", description: "intro_lesson", price: 45.00, duration: 60, user_id: user.id )}
+  let(:slot) { Slot.create(start_time: '08:00:00', end_time: '09:00:00', lesson_id: lesson.id ) }
+  let(:booking) { Booking.create(booking_date: '2023-09-10', lesson_order_id: lesson_order.id ) }
+
+  # slots cannot have the same start_time under the same booking_id
+  describe "validations" do
+    it { should validate_presence_of(:start_time) }
+    it { should validate_presence_of(:end_time) }
+
+    it 'validates the uniqueness of start_time per booking_date' do
+      slot_2 = Slot.create(start_time: '08:00:00', end_time: '09:00:00', lesson_id: lesson.id )
+      expect(slot).to be_valid
+      expect(slot_2).not_to be_valid
+      expect(slot_2.errors[:start_time]).to include('has already been taken')
+    end
+  end
+
+  describe "associations" do
+    it { should belong_to(lesson) }
+    it { should have_many(:bookings).through(:booking_slots) }
+  end
+end
